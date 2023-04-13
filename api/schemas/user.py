9d6311c3da8d2f1ad55cf58356f4fa51b1856be0
arvindinh <arvindinh0814@gmail.com>
2023-04-12 22:12:06 -0400

    update user to include image url

--- api/schemas/user.py
+++ api/schemas/user.py
@@ -1,10 +1,11 @@
 class User:
-    def __init__(self, user_id, username, tracks, artists=None, playlists=None, friends=None, friend_requests=None):
+    def __init__(self, user_id, username, tracks, image_url, artists=None, playlists=None, friends=None, friend_requests=None):
         self.user_id = user_id
         self.username = username
         self.friends = friends if friends is not None else []
         self.friend_requests = friend_requests if friend_requests is not None else []
         self.tracks = tracks
+        self.image_url = image_url
         self.artists = artists if artists is not None else []
         self.playlists = playlists if playlists is not None else []
 
@@ -13,6 +14,7 @@ class User:
             "user_id": str(self.user_id),
             "username": self.username,
             "tracks": self.tracks,
+            "image_url": self.image_url,
             "artists": self.artists,
             "playlists": [playlist.to_dict() for playlist in self.playlists],
             "friends": [friend.to_dict() for friend in self.friends],
