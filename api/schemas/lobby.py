553aa676c1e6d38e6d23bfbdd892a3c97dcc20ee
espkz <52008233+espkz@users.noreply.github.com>
2023-04-16 19:34:45 +0000

    adjust lobby schema

--- api/schemas/lobby.py
+++ api/schemas/lobby.py
@@ -9,7 +9,7 @@ class Lobby():
 
     def __init__(self, ogboi):
         """"
-        :param ogboi: user object, first person who created lobby and will be the original creator of the playlist
+        :param ogboi: str of user_id, first person who created lobby and will be the original creator of the playlist
         """
         self.lobbycode = ""
         """
@@ -24,8 +24,7 @@ class Lobby():
         generate a unique lobby code
         hash userID to create
         """
-        firstuser = list(self.users.keys())[0]
-        key = firstuser.user_id
+        key = list(self.users.keys())[0]
         key = hashlib.sha256(key.encode())
         key = key.hexdigest()
         return key
@@ -33,14 +32,14 @@ class Lobby():
     def addNewUser(self, newuser):
         """
         function for adding new user to the lobby
-        :param newuser: user object, a new user who joins the lobby
+        :param newuser: str, id of a new user who joins the lobby
         """
         self.users[newuser] = ""
 
     def deleteUser(self, user):
         """
         function for adding new user to the lobby
-        :param user: user object, user to delete
+        :param user: str, id of a user who is in the lobby
         """
         self.users.pop(user)
 
@@ -48,32 +47,29 @@ class Lobby():
         """
         function for returning just the users
         """
-        userObjects = []
-        for user in self.users.keys():
-            userObjects.append(user)
-        return userObjects
+        return self.users.keys()
 
     def returnInputs(self):
         """
         function for returning just the GPT inputs
         """
-        gptinputs = []
-        for input in self.users.values():
-            gptinputs.append(input)
-        return gptinputs
+        return self.users.values()
 
 
     def updateGPTInput(self, user, input):
         """
         function to add/update GPT input to respective user
-        :param user: user object
+        :param user: str, id of a user 
         :param input: str, user input for GPT
         """
         self.users[user] = input
 
-    def to_dict(self):
+    def to_dict(self, user):
+        """
+        function to return dictionary version of a user in lobby
+        :param user: user id string
+        """
         return {
-            "lobby_code": self.lobbycode,
-            "users" : self.returnUsers(),
-            "gpt_inputs" : self.returnInputs()
+            "user_id" : user,
+            "gpt_input" : self.users[user]
         }
