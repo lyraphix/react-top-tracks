ad1bcbf58745c3f9c2ebf2c7c9ec3265ae96316d
FredHino <fredhinojosa03@gmail.com>
2023-04-18 18:17:49 -0400

    changed openai model to 3.5 turbo

--- api/helpers/openai_helper.py
+++ api/helpers/openai_helper.py
@@ -8,22 +8,28 @@ class PlaylistMakerGPT:
 
     def get_artists_from_input(self, user_input, top_artists):
         # Get the artist list from ChatGPT
-        prompt = f"Your output will be a list of artists names. Based on the meaning and vibe of this phrase '{user_input}', rank the top 3 artists from this list '{top_artists}', that match the phrase the most. If an artist's name is included in the phrase, include the artist too."
-        response = openai.Completion.create(engine="text-babbage-001", prompt=prompt, max_tokens=500, n=1, stop=None, temperature=0.7)
-        print("Response 1:")
-        print(response)
-        artist_list_text_2 = response.choices[0].text.strip()
+        messages = [
+        {
+            "role": "user",
+            "content": f"Your output will be a list of artists names. Based on the meaning and vibe of this phrase '{user_input}', output a list of 3 artists you think are related to the phrase. If an artist's name is included in the phrase, include the artist too."
+        }
+        ]
+        response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
+        artist_list_text_2 = response.choices[0].message['content'].strip()
         artist_text_lines_2 = artist_list_text_2.split('\n')
 
         artist_list_2 = [line.partition('. ')[-1].strip().rstrip('"') for line in artist_text_lines_2 if line]
         artist_list_2 = [line.partition('. ')[-1].strip().replace('"', '') for line in artist_text_lines_2 if line]
         artist_list_2 = [line.partition('. ')[-1].strip().replace('.', '') for line in artist_text_lines_2 if line]
 
-        prompt = f"Your output will be a list of 3 artists names only. Based on the meaning and vibe of this phrase '{user_input}' and these artists '{artist_list_2}' output a list of 3 artists you think are related to the phrase and artists provided. "
-        response = openai.Completion.create(engine="text-babbage-001", prompt=prompt, max_tokens=500, n=1, stop=None, temperature=0.7)
-        print("Response 2:")
-        print(response)
-        artist_list_text_1 = response.choices[0].text.strip()
+        messages = [
+        {
+            "role": "user",
+            "content": f"Your output will be a list of artists names. Based on the meaning and vibe of this phrase '{user_input}', rank the top 3 artists from this list '{top_artists}', that match the phrase the most. If an artist's name is included in the phrase, include the artist too."
+        }
+        ]
+        response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
+        artist_list_text_1 = response.choices[0].message['content'].strip()
         artist_text_lines_1 = artist_list_text_1.split('\n')
         artist_list_1 = [line.partition('. ')[-1].strip().rstrip('"') for line in artist_text_lines_1 if line]
         artist_list_1 = [line.partition('. ')[-1].strip().replace('"', '') for line in artist_text_lines_1 if line]
