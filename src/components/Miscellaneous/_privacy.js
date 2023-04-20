54fee4b8abf353352381c7be5928590c2739eb8c
espkz <52008233+espkz@users.noreply.github.com>
2023-04-20 05:24:10 +0000

    privacy policy adjustments

--- src/components/Miscellaneous/_privacy.js
+++ src/components/Miscellaneous/_privacy.js
@@ -22,21 +22,26 @@ const Privacy = () => {
                             </span>
 
                             <span className={styles.gettheplaylistjus} style={{ marginTop: -20, fontSize: 15, lineHeight: 1.5 }}>
+                                <br /> 
                                 Information Collected <br /> 
                                 We collect personal information that you provide us when using our web app. This includes your Spotify account information, including but not limited to your listening history, playlists, and other data related to your account, and any custom phrases or keywords used to filter your playlist. 
                                 We also collect certain non-personal information such as your IP address, browser type, and device information to help us optimize our web app and improve your experience.
+                                <br /> 
                                 <br /> 
                                 Use of Information<br /> 
                                 We use the information we collect to provide you with our web app and its features, including creating playlists and filtering songs based on your custom phrases.
                                 We may also use your information to communicate with you about updates, changes, or improvements to our web app.
                                 We do not share or sell your personal information to third parties unless required by law or as necessary to provide our web app and its features.
                                 <br /> 
+                                <br /> 
                                 Data Security <br /> 
                                 We take the security of your personal information seriously and use reasonable measures to protect it from unauthorized access, alteration, or disclosure.
                                 However, please be aware that no transmission of data over the internet is completely secure and we cannot guarantee the security of your personal information.
                                 <br /> 
+                                <br /> 
                                 Updates to Policy <br /> 
                                 We reserve the right to update or modify this Policy at any time without prior notice. Your continued use of our web app following any such changes constitutes your acceptance of the revised Policy.
+                                <br /> 
                                 <br /> 
                                 Contact Us <br /> 
                                 If you have any questions or concerns about our Privacy Policy or the way we handle your personal information, please contact us.
